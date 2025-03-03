48fb2085f12c0926656725af6d59001e85233154
henmeh <143960925+henmeh@users.noreply.github.com>
2025-03-03 16:23:45 +0100

    App.txs::added useState to the frontend

--- src/Frontend/bitcoin-dashboard/src/App.tsx
+++ src/Frontend/bitcoin-dashboard/src/App.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react"
 import { dummyTodos } from "./data/todos"
 import TodoItem from "./components/TodoItem"
 
 function App() {
 
-  
+  const [todos, setTodos] = useState(dummyTodos);
+
+  function setToDoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) => prevTodos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed }
+      }
+      return todo;
+    }
+    ))
+  }
+
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3xl text-center text-amber-500">My ToDo's</h1>
       <div className="max-w-lg mx-auto">
         <div>
-          {dummyTodos.map((todo) => <TodoItem todo={todo}/> )}
+          {todos.map((todo) => <TodoItem 
+                                      key={todo.id}
+                                      todo={todo}
+                                      onCompletedChange={setToDoCompleted}/> )}
         </div>
       </div>
     </main>
--- src/Frontend/bitcoin-dashboard/src/components/TodoItem.tsx
+++ src/Frontend/bitcoin-dashboard/src/components/TodoItem.tsx
@@ -2,13 +2,19 @@ import { dummyTodo } from "../types/todos";
 
 interface TodoItemProps {
     todo: dummyTodo;
+    onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function TodoItem({todo}: TodoItemProps) {
+export default function TodoItem({todo, onCompletedChange}: TodoItemProps) {
   return (
     <div className="bg-amber-100 p-4 m-2 rounded-lg">
-      <h2 className="text-xl">{todo.title}</h2>
-      <p>{todo.completed ? "Completed" : "Not Completed"}</p>
+        <label className="flex items-center gap-2">
+            <input type="checkbox"
+            checked={todo.completed}
+            onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
+            />
+            <span className={todo.completed ? "line-through text-gray-400" : ""}>{todo.title}</span>
+        </label>
     </div>
   )
 }
