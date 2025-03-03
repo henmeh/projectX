24e10b7a3f5a70ea8af8eb86707ecfabdd5dd07e
henmeh <143960925+henmeh@users.noreply.github.com>
2025-03-03 16:52:34 +0100

    App.tsx::added function to create new Todo

--- src/Frontend/bitcoin-dashboard/src/App.tsx
+++ src/Frontend/bitcoin-dashboard/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { dummyTodos } from "./data/todos"
 import TodoItem from "./components/TodoItem"
+import AddTodoForm from "./components/AddTodoForm";
 
 function App() {
 
@@ -16,10 +17,21 @@ function App() {
     ))
   }
 
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },     
+      ...prevTodos]);
+  }
+
   return (
     <main className="py-10 h-screen">
       <h1 className="font-bold text-3xl text-center text-amber-500">My ToDo's</h1>
       <div className="max-w-lg mx-auto">
+      <AddTodoForm onSubmit={addTodo}/>
         <div>
           {todos.map((todo) => <TodoItem 
                                       key={todo.id}
